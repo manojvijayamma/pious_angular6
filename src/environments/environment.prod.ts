c32b31d781e8177351026a8de79a1c2dac694b6c
manoj vijayan <manojvijayamma@gmail.com>
2019-02-16 08:00:40 +0530

    dashboard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,10 @@ export const environment = {
   //api_url: 'http://weonws.com/sellers/public/api/',
   //image_url:'http://weonws.com/sellers/public/uploads/',
 
-  api_url: 'http://18.220.111.165/tms/public/api/',
-  image_url:'http://18.220.111.165/tms/public/uploads/',
+  //api_url: 'http://18.220.111.165/tms/public/api/',
+ // image_url:'http://18.220.111.165/tms/public/uploads/',
+
+  api_url: 'http://store.safeertyres.com/storeApp/public/api/',
+  image_url:'http://store.safeertyres.com/storeApp/public/uploads/',
+
 };
